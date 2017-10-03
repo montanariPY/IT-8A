c7001117c25b95bc9ef5019b4528106027c58195
montanariPY <31954440+montanariPY@users.noreply.github.com>
2017-10-03 11:16:02 +0200

    montanari
    Update folders 2

---
+++ MoegelToender/Laurens.py
@@ -0,0 +1,41 @@
+Exercise 1
+
+print("Hello, World!")
+
+Exercise 2
+
+mystring = "hello"
+myfloat = 10.0
+myint = 20
+if mystring == "hello":
+    print("String: %s" % mystring)
+if isinstance(myfloat, float) and myfloat == 10.0:
+    print("Float: %f" % myfloat)
+if isinstance(myint, int) and myint == 20:
+    print("Integer: %d" % myint)
+	
+Exercise 3 
+
+numbers = []
+strings = []
+names = ["John", "Eric", "Jessica"]
+
+# write your code here
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+
+strings.append("hello")
+strings.append("world")
+
+second_name = names[1]
+
+# this code should write out the filled arrays and the second name in the names list (Eric).
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
+
+Exercise 4
+
+
+	
